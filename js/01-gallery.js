6cc9866ba90edd4f1c8ea912f0ad33c42c6e51bd
Olena Novitska <109989618+OlenaNov@users.noreply.github.com>
2023-01-07 12:08:32 +0100

    change-author-name

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,31 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+// // Add imports above this line
+// import { galleryItems } from './gallery-items';
+// // Change code below this line
 
-console.log(galleryItems);
+// import SimpleLightbox from "simplelightbox";
+// import "simplelightbox/dist/simple-lightbox.min.css";
+
+// console.log(galleryItems);
+
+// const gallery = document.querySelector('.gallery');
+
+// const addItems = galleryItems.reduce((acc, { original, preview, description }) => 
+//     acc += `<a class="gallery__item" href="${original}">
+//             <img 
+//                 class="gallery__image"
+//                 src="${preview}"
+//                 alt="${description}"
+//                 title="${description}" 
+//             />
+//         </a>`, "");
+
+// gallery.insertAdjacentHTML('beforeend', addItems);
+
+//     var lightbox = new SimpleLightbox('.gallery a', { 
+//         captionsData: `alt`,
+//         animationSpeed:	250,
+// });
+
+// console.log(SimpleLightbox);
+
+console.log('HELLO');
